233d4d6b3c542cc0d7239888f72955738a5e85c6
Carlos Buenrostro <c.buenrostro@blackstone.studio>
2021-03-20 16:32:21 -0600

    Create and get all endpoint working

--- src/core/user/user.controller.ts
+++ src/core/user/user.controller.ts
@@ -1,18 +1,19 @@
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { User } from '.prisma/client';
-import { Controller, Post, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { IResponseBase } from '../interfaces/types';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
-  create(user: User) {
-    return this.userService.create(user);
+  create(@Body() body: User): Promise<IResponseBase> {
+    return this.userService.create(body);
   }
 
   @Get()
-  getAll() {
+  getAll(): Promise<IResponseBase> {
     return this.userService.getAll();
   }
 }
